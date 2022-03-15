8d45cf5ebc5b2738bd32a297201b7e1e43095683
Obvees3 <101640077+obvees@users.noreply.github.com>
2022-03-15 12:35:33 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    int wiek;
+    wiek = 22;
+    System.out.println("Mam "+wiek+" lata");
+    System.out.println("Urodzilem sie "+wiek+" lat temu");
+    System.out.println("Za rok skoncze "+(wiek+1));
+      
+  }
+}
